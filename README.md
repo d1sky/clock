a6045423e1aff10f930a879ef058ef3e63281856
Denis Retinsky <godasin@gmail.com>
2021-10-02 22:46:24 +0300

    short digital with shadow

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # clock
 reflections on clock
+
+Угол поворота всех стрелок за 1 секунду:
+    *Секундная стрелка повернется на угол — (1/60)*360o = 6o
+    *Минутная стрелка повернется на угол — (1/60)*6o = 0,1o
+    *Часовая стрелка повернется на угол — (1/60)*0,1o ≈ 0,0017o
+
+var t_sec = 6*d.getSeconds();  //Определяем угол для секунд
+var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds()); //Определяем угол для минут
+var t_hour = 30*(d.getHours() + (1/60)*d.getMinutes());  //Определяем угол для часов
+
+lengthSeconds*Math.cos(Math.PI/2 - t_sec*(Math.PI/180))
+lengthSeconds*Math.sin(Math.PI/2 - t_sec*(Math.PI/180))
--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 window.onload = () => {
     window.setInterval(() => {
-         var now = new Date();
-          var clock = document.getElementById("clock");
-        clock.innerHTML = now.toLocaleTimeString();
+        var now = new Date();
+        var clock = document.getElementById("clock");
+        clock.innerHTML = now.toLocaleTimeString([], {timeStyle: 'short'});
+
+        var t_sec = 6*now.getSeconds();  //Определяем угол для секунд
+
+        console.log(t_sec);
+        console.log(Math.cos(Math.PI/2 - t_sec*(Math.PI/180))*10);
+        console.log(Math.sin(Math.PI/2 - t_sec*(Math.PI/180))*10);
+
+        var x = Math.trunc(Math.sin(Math.PI/2 - t_sec*(Math.PI/180) + 90)*10)*2
+        var y = Math.trunc(Math.cos(Math.PI/2 - t_sec*(Math.PI/180) + 90)*10)*2
+
+        clock.style.textShadow = `${x}px ${y}px 10px  rgba(0, 0, 0, 0.5)`;
     }, 1000);
-   };
+};
