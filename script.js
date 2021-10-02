249e443e021594b03ed6c028f679dd241b14d83e
Denis Retinsky <godasin@gmail.com>
2021-10-02 22:11:38 +0300

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    window.setInterval(() => {
+         var now = new Date();
+          var clock = document.getElementById("clock");
+        clock.innerHTML = now.toLocaleTimeString();
+    }, 1000);
+   };
