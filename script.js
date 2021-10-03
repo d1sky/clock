b467de544de9a0f3fd5c7669f4dd7dcc42fbc93d
Denis Retinsky <godasin@gmail.com>
2021-10-03 19:56:24 +0300

    correct shadow direction

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ window.onload = () => {
     window.setInterval(() => {
         var now = new Date();
         var clock = document.getElementById("clock");
-        clock.innerHTML = now.toLocaleTimeString([], {timeStyle: 'short'});
+        var info = document.getElementById("info");
+        clock.innerHTML = now.toLocaleTimeString([], { timeStyle: 'short' });
 
-        var t_sec = 6*now.getSeconds();  //Определяем угол для секунд
+        var t_sec = 6 * now.getSeconds();  //Определяем угол для секунд
 
-        console.log(t_sec);
-        console.log(Math.cos(Math.PI/2 - t_sec*(Math.PI/180))*10);
-        console.log(Math.sin(Math.PI/2 - t_sec*(Math.PI/180))*10);
+        var x = Math.round(Math.sin(Math.PI - t_sec * (Math.PI / 180))*10);
+        var y = Math.round(Math.cos(Math.PI - t_sec * (Math.PI / 180))*10);
 
-        var x = Math.trunc(Math.sin(Math.PI/2 - t_sec*(Math.PI/180) + 90)*10)*2
-        var y = Math.trunc(Math.cos(Math.PI/2 - t_sec*(Math.PI/180) + 90)*10)*2
+        info.innerHTML = now.toLocaleTimeString() + ' ' + x + ' ' + y;
 
         clock.style.textShadow = `${x}px ${y}px 10px  rgba(0, 0, 0, 0.5)`;
+
     }, 1000);
+
+    for (let i = 0; i < 60; i++) {
+        t_sec = i * 6;
+        var x = Math.round(Math.sin(Math.PI - t_sec * (Math.PI / 180))*10);
+        var y = Math.round(Math.cos(Math.PI - t_sec * (Math.PI / 180))*10);
+
+        console.log(i, x, y);
+    }
 };
